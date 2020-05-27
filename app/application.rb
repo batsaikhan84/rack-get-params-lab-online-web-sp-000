bb2d486673148af01e45d7a53b55003c32927755
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-27 22:11:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -10,6 +10,21 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
+    elsif req.path.match(/cart/)
+      if !@@cart.empty?
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+      
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@item.include?(search_term)
+        @@
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
